0d3e760eb4090d1515ade2ce9504bb199801703f
ButenkoDmytro <137228052+ButenkoDmytro@users.noreply.github.com>
2023-06-21 00:24:51 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float x;
+    cout << "x" << endl;
+    cout << "Enter the number: ";
+    cin >> x;
+    cout << "Output number: " << x*x << endl;
+    return 0;
+}
